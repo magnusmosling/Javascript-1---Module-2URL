a60ff4a6d59ba3d9fa785ad44b2e43f4a08d575d
Magnus <magnusmosling@live.no>
2022-02-22 21:06:21 +0100

    working on

--- Lesson 2.2/script/script.js
+++ Lesson 2.2/script/script.js
@@ -4,13 +4,65 @@
 
 
 
+
+// const button = document.querySelector(".button");
+
+// button.onclick = function(){
+//     console.log("I am a button");
+// }
+
+
+
+
+
+
+
+// function button(){
+//     document.querySelector().innerHTML = count;
+// }
+
+// function count(){
+//     const newInterval = setInterval(function(){
+//         count+=1
+//         countSpan.innerHTML=count
+
+//         if (count === 7) {
+//             clearInterval(newInterval)   
+//         }
+//     }, Interval);
+
+// }
+
+
+
+
+
+
+
+
 const button = document.querySelector('button');
 const countSpan = document.querySelector(".count")
 let count = 0
 
+// button.onclick = count();
+
+// function count(){
+//     const countSpan = document.querySelector(".count")
+//     let count = 0
+//     const newInterval = setInterval(function(){
+//         count+=1
+//         countSpan.innerHTML=count
+
+//         if (count === 7) {
+//             clearInterval(newInterval)   
+//         }
+//     }, Interval);
+
+// }
 
 
 
+let Interval = 1000;
 
 const myInterval = function(){
     const newInterval = setInterval(function(){
@@ -20,8 +72,8 @@ const myInterval = function(){
     if (count === 7) {
         clearInterval(newInterval)   
     }
-}, 1000);
+}, Interval);
 }
 
 
-button.onclick = myInterval();
+
